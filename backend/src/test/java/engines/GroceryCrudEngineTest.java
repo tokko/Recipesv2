5573f9613aa869b1e4dbef0c0cfc007de3538d95
tokko <andreg@kth.se>
2015-07-31 08:33:40 +0200

    test case for list groceries

--- backend/src/test/java/engines/GroceryCrudEngineTest.java
+++ backend/src/test/java/engines/GroceryCrudEngineTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 import util.TestsWithObjectifyStorage;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class GroceryCrudEngineTest extends TestsWithObjectifyStorage {
     RecipeUser user;
@@ -26,6 +27,9 @@ public class GroceryCrudEngineTest extends TestsWithObjectifyStorage {
         ofy.save().entity(user).now();
         groceryList = Arrays.asList(new Grocery("Grocery1", user), new Grocery("Grocery2", user), new Grocery("Grocery3", user));
         ofy.save().entities(groceryList).now();
+
+        RecipeUser otherUser = new RecipeUser("email1");
+        ofy.save().entities(new Grocery("Grocery4", otherUser)).now();
         groceryCrudEngine = new GroceryCrudEngine();
     }
 
@@ -34,5 +38,13 @@ public class GroceryCrudEngineTest extends TestsWithObjectifyStorage {
         List<Grocery> groceries = groceryCrudEngine.listGroceries(user);
         assertEquals(groceryList.size(), groceries.size());
         assertEquals(groceryList, groceries);
+    }
+
+    @Test
+    public void testListGroceries_OnlyUsersGroceriesReturned() {
+        List<Grocery> groceries = groceryCrudEngine.listGroceries(user);
+        for (Grocery g : groceries) {
+            assertTrue(groceryList.contains(g));
+        }
     }
 }
