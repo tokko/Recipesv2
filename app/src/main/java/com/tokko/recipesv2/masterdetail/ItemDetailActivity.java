89d744455f189f9f2deb0be804faaba5011f8dac
tokko <andreg@kth.se>
2015-08-03 03:34:05 +0200

    detailactivity mounts fragment properly

--- app/src/main/java/com/tokko/recipesv2/masterdetail/ItemDetailActivity.java
+++ app/src/main/java/com/tokko/recipesv2/masterdetail/ItemDetailActivity.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.google.inject.Key;
+import com.google.inject.util.Types;
 import com.tokko.recipesv2.R;
+
+import roboguice.RoboGuice;
 
 /**
  * An activity representing a single Item detail screen. This
@@ -22,33 +26,12 @@ public class ItemDetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
-
-        // Show the Up button in the action bar.
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-
-        // savedInstanceState is non-null when there is fragment state
-        // saved from previous configurations of this activity
-        // (e.g. when rotating the screen from portrait to landscape).
-        // In this case, the fragment will automatically be re-added
-        // to its container so we don't need to manually add it.
-        // For more information, see the Fragments API guide at:
-        //
-        // http://developer.android.com/guide/components/fragments.html
-        //
-        if (savedInstanceState == null) {
-            // Create the detail fragment and add it to the activity
-            // using a fragment transaction.
-            Bundle arguments = new Bundle();
-            arguments.putString(ItemDetailFragment.ARG_ITEM_ID,
-                    getIntent().getStringExtra(ItemDetailFragment.ARG_ITEM_ID));
-            /*
-            ItemDetailFragment fragment = new ItemDetailFragment();
-            fragment.setArguments(arguments);
-            getFragmentManager().beginTransaction()
-                    .add(R.id.item_detail_container, fragment)
-                    .commit();
-                    */
-        }
+        if (getActionBar() != null)
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        Class<?> entityClass = (Class<?>) getIntent().getSerializableExtra(ItemDetailFragment.EXTRA_CLASS);
+        ItemDetailFragment fragment = (ItemDetailFragment) RoboGuice.getInjector(this).getInstance(Key.<ItemDetailFragment>get(Types.newParameterizedType(ItemDetailFragment.class, entityClass)));
+        fragment.setArguments(getIntent().getExtras());
+        getFragmentManager().beginTransaction().replace(android.R.id.content, fragment).commit();
     }
 
     @Override
--- app/src/test/java/com/tokko/recipesv2/ItemDetailActivityTests.java
+++ app/src/test/java/com/tokko/recipesv2/ItemDetailActivityTests.java
@@ -1,6 +1,7 @@
 package com.tokko.recipesv2;
 
 import android.content.Intent;
+import android.widget.TextView;
 
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.json.GenericJson;
@@ -8,10 +9,12 @@ import com.google.api.client.json.JsonString;
 import com.google.api.client.util.Key;
 import com.tokko.recipesv2.backend.entities.groceryApi.model.Grocery;
 import com.tokko.recipesv2.masterdetail.ItemDetailActivity;
+import com.tokko.recipesv2.masterdetail.ItemDetailFragment;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
@@ -22,22 +25,24 @@ import static org.junit.Assert.assertEquals;
 @Config(constants = BuildConfig.class, sdk = 21, manifest = "/app/src/main/AndroidManifest.xml")
 public class ItemDetailActivityTests {
 
+    private ItemDetailActivity activity;
+    private Grocery g;
+
     @Before
     public void setUp() throws Exception {
-
         Intent i = new Intent(RuntimeEnvironment.application, ItemDetailActivity.class);
+        g = new Grocery();
+        g.setId(1L);
+        g.setTitle("Title");
+        i.putExtra(ItemDetailFragment.EXTRA_CLASS, Grocery.class);
+        i.putExtra(ItemDetailFragment.EXTRA_ENTITY, new AndroidJsonFactory().toString(g));
+        activity = Robolectric.buildActivity(ItemDetailActivity.class).withIntent(i).create().start().resume().visible().get();
     }
 
     @Test
-    public void testGrocery() throws Exception {
-        Grocery grocery = new Grocery();
-        grocery.setId(1L);
-        grocery.setTitle("Title");
-        String json = new AndroidJsonFactory().toString(grocery);
-
-        Grocery grocery1 = new AndroidJsonFactory().fromString(json, Grocery.class);
-        assertEquals(grocery.getId(), grocery1.getId());
-        assertEquals(grocery.getTitle(), grocery1.getTitle());
+    public void testGroceryDetails_TitleDisplayed() throws Exception {
+        String s = ((TextView) activity.findViewById(R.id.grocery_title)).getText().toString();
+        assertEquals(g.getTitle(), s);
     }
 
     private class Tester extends GenericJson {
