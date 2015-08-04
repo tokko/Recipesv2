13024f0b4a82845d9202d8c020e2564dfe964b03
tokko <andreg@kth.se>
2015-08-04 10:57:32 +0200

    test disable keyguard

--- app/src/main/java/com/tokko/recipesv2/RecipeApplication.java
+++ app/src/main/java/com/tokko/recipesv2/RecipeApplication.java
@@ -1,6 +1,8 @@
 package com.tokko.recipesv2;
 
 import android.app.Application;
+import android.app.KeyguardManager;
+import android.content.Context;
 
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
 import com.tokko.recipesv2.groceries.GroceryGuiceModule;
@@ -15,5 +17,11 @@ public class RecipeApplication extends Application {
         super.onCreate();
         ApiFactory.credential = GoogleAccountCredential.usingAudience(getApplicationContext(), "server:client_id:826803278070-0gs94ct68qhnn9b2tpi1mnuptu5al77n.apps.googleusercontent.com");
         RoboGuice.overrideApplicationInjector(this, new MasterDetailGuiceModule(), new GroceryGuiceModule());
+        if (BuildConfig.DEBUG) {
+            KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+
+            KeyguardManager.KeyguardLock keyguardLock = km.newKeyguardLock("TAG"); //TODO: fix this
+            keyguardLock.disableKeyguard();
+        }
     }
 }
