99ddfaf8cbcb4f2ca472e93df1dd5c9344304056
tokko <andreg@kth.se>
2015-08-12 06:46:48 +0200

    increment backend api version in constants file

---
+++ backend/src/test/java/util/ConstantsTest.java
@@ -0,0 +1,14 @@
+package util;
+
+import org.junit.Test;
+
+/**
+ * Created by andre_000 on 2015-08-12.
+ */
+public class ConstantsTest {
+
+    @Test
+    public void testConstants() {
+
+    }
+}
