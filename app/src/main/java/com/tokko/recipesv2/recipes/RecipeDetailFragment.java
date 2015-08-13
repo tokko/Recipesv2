dec4e505b6635e9be2a860b1bc406229fe49a0df
tokko <andreg@kth.se>
2015-08-13 21:57:00 +0200

    recipefragment implemented mostly

--- app/src/main/java/com/tokko/recipesv2/recipes/RecipeDetailFragment.java
+++ app/src/main/java/com/tokko/recipesv2/recipes/RecipeDetailFragment.java
@@ -1,18 +1,41 @@
 package com.tokko.recipesv2.recipes;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.inject.Inject;
+import com.tokko.recipesv2.R;
 import com.tokko.recipesv2.backend.entities.recipeApi.model.Recipe;
 import com.tokko.recipesv2.masterdetail.ItemDetailFragment;
+import com.tokko.recipesv2.views.EditTextViewSwitchable;
 
 public class RecipeDetailFragment extends ItemDetailFragment<Recipe> {
 
+    @Inject
+    private EditTextViewSwitchable title;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.recipedetailfragment, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        title.setHint("Title");
+    }
+
     @Override
     protected void bindView(Recipe entity) {
-
+        title.setData(entity.getTitle());
     }
 
     @Override
     protected Recipe getEntity() {
-        return null;
+        entity.setTitle(title.getData());
+        return entity;
     }
 
     @Override
