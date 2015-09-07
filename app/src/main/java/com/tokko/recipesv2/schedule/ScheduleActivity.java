1989d6c429018351a55cc5550986036b6d683153
tokko <andreg@kth.se>
2015-09-07 21:35:31 +0200

    scheduleactivity

--- app/src/main/java/com/tokko/recipesv2/schedule/ScheduleActivity.java
+++ app/src/main/java/com/tokko/recipesv2/schedule/ScheduleActivity.java
@@ -10,27 +10,27 @@ import roboguice.activity.RoboActivity;
 
 public class ScheduleActivity extends RoboActivity {
 
+    private RecipeListFragment recipeFragment;
+    private ScheduleFragment scheduleFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
+        setContentView(R.layout.scheduleactivity);
+        recipeFragment = (RecipeListFragment) getFragmentManager().findFragmentById(R.id.recipeListFragment);
+        scheduleFragment = (ScheduleFragment) getFragmentManager().findFragmentById(R.id.scheduleFragment);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_schedule, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
