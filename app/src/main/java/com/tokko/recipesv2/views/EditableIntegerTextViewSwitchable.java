9b01a5918010bfac90547b2b1c41b7e637d8cebd
tokko <andreg@kth.se>
2015-09-10 20:09:16 +0200

    helpings

--- app/src/main/java/com/tokko/recipesv2/views/EditableIntegerTextViewSwitchable.java
+++ app/src/main/java/com/tokko/recipesv2/views/EditableIntegerTextViewSwitchable.java
@@ -3,10 +3,42 @@ package com.tokko.recipesv2.views;
 import android.content.Context;
 import android.text.InputType;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class EditableIntegerTextViewSwitchable extends EditTextViewSwitchable {
+public class EditableIntegerTextViewSwitchable extends EditableViewSwitchable<TextView, EditText, Integer> {
     public EditableIntegerTextViewSwitchable(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        super(context, attrs, new TextView(context, attrs), new EditText(context, attrs));
         edit.setInputType(InputType.TYPE_CLASS_NUMBER);
+        edit.setVisibility(View.GONE);
+    }
+
+    public EditableIntegerTextViewSwitchable setHint(String hint) {
+        edit.setHint(hint);
+        return this;
+    }
+
+    @Override
+    protected void onDiscard() {
+        edit.setText(label.getText());
+    }
+
+    @Override
+    protected void onAccept() {
+        label.setText(edit.getText());
+    }
+
+    @Override
+    public Integer getData() {
+        return Integer.valueOf(label.getText().toString());
+    }
+
+    @Override
+    public void setData(Integer data) {
+        if (data != null) {
+            edit.setText(data.toString());
+            label.setText(data.toString());
+        }
     }
 }
