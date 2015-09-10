97dadd4e1a602527deac47e39bb2d094109f02ad
tokko <andreg@kth.se>
2015-09-10 22:33:43 +0200

    more advanced filtering in scheduleentries

--- backend/src/main/java/com/tokko/recipesv2/backend/resourceaccess/ScheduleEntryRa.java
+++ backend/src/main/java/com/tokko/recipesv2/backend/resourceaccess/ScheduleEntryRa.java
@@ -10,13 +10,16 @@ import static com.tokko.recipesv2.backend.resourceaccess.OfyService.ofy;
 public class ScheduleEntryRa {
 
     public List<ScheduleEntry> getScheduleEntries(long date, RecipeUser user){
-        List<ScheduleEntry> list = ofy().load().type(ScheduleEntry.class).ancestor(user).filter("date >=", date).list();
+        return getScheduleEntries(date, Long.MAX_VALUE, user);
+    }
+
+    public List<ScheduleEntry> getScheduleEntries(long startDate, long endDate, RecipeUser user) {
+        List<ScheduleEntry> list = ofy().load().type(ScheduleEntry.class).ancestor(user).filter("date >=", startDate).filter("date <=", endDate).list();
         for (ScheduleEntry se : list) {
             se.load();
         }
         return list;
     }
-
     public ScheduleEntry commitEntry(ScheduleEntry entry, RecipeUser user){
         entry.setUser(user);
         entry.prepare();
