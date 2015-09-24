4daa30b50052f3a6bd49753918c2cb53ce158354
tokko <andreg@kth.se>
2015-09-24 19:54:20 +0200

    rescale to thirds

--- backend/src/test/java/unit/engines/RecipeRescaleEngineTests.java
+++ backend/src/test/java/unit/engines/RecipeRescaleEngineTests.java
@@ -31,4 +31,22 @@ public class RecipeRescaleEngineTests {
         assertEquals(4.4, rescaledIngredients.get(0).getQuantity().getQuantity(), 0);
         assertEquals(2.6, rescaledIngredients.get(1).getQuantity().getQuantity(), 0);
     }
+
+    @Test
+    public void testRescaleIngredients_toThirds() throws Exception {
+        Ingredient i1 = new Ingredient();
+        i1.setQuantity(new Quantity(3));
+        Ingredient i2 = new Ingredient();
+        i2.setQuantity(new Quantity(6));
+        List<Ingredient> ingredients = Arrays.asList(i1, i2);
+
+        RecipeRescaleEngine engine = new RecipeRescaleEngine();
+
+        List<Ingredient> rescaledIngredients = engine.rescaleIngredientsToNumberOfHelpings(ingredients, 3, 1);
+
+        assertNotNull(rescaledIngredients);
+        assertEquals(ingredients.size(), rescaledIngredients.size());
+        assertEquals(1, rescaledIngredients.get(0).getQuantity().getQuantity(), 0);
+        assertEquals(2, rescaledIngredients.get(1).getQuantity().getQuantity(), 0);
+    }
 }
