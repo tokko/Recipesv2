5b6e72bd1c0c24170ce8738451d427360737f094
tokko <andreg@kth.se>
2015-09-24 20:27:22 +0200

    rescale recipe to fractions

--- backend/src/main/java/com/tokko/recipesv2/backend/engines/RecipeRescaleEngine.java
+++ backend/src/main/java/com/tokko/recipesv2/backend/engines/RecipeRescaleEngine.java
@@ -8,6 +8,17 @@ public class RecipeRescaleEngine {
 
     public List<Ingredient> rescaleIngredientsToNumberOfHelpings(List<Ingredient> ingredients, int fromHelpings, int toHelpings) {
         //TODO: This is completely borked. What the fuck? Should calculate one helping and then multiply på toHelpings
+        for (Ingredient ingredient : ingredients) {
+            double quantity = ingredient.getQuantity().getQuantity();
+            double normalized = quantity / (double) fromHelpings;
+            double newQuantity = normalized * (double) toHelpings;
+            ingredient.getQuantity().setQuantity(newQuantity);
+        }
+        return ingredients;
+    }
+
+    public List<Ingredient> rescaleIngredientsToNumberOfHelpings2(List<Ingredient> ingredients, int fromHelpings, int toHelpings) {
+        //TODO: This is completely borked. What the fuck? Should calculate one helping and then multiply på toHelpings
         double fraction = ((double) toHelpings) / ((double) fromHelpings);
         for (Ingredient ingredient : ingredients) {
             ingredient.getQuantity().setQuantity(ingredient.getQuantity().getQuantity() * fraction);
--- backend/src/test/java/unit/engines/RecipeRescaleEngineTests.java
+++ backend/src/test/java/unit/engines/RecipeRescaleEngineTests.java
@@ -5,24 +5,50 @@ import com.tokko.recipesv2.backend.entities.Ingredient;
 import com.tokko.recipesv2.backend.units.Quantity;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+@RunWith(value = Parameterized.class)
 public class RecipeRescaleEngineTests {
 
+    private final int from;
+    private final int to;
+    private final double[] params;
+    private final double[] expected;
+
+    public RecipeRescaleEngineTests(int from, int to, double[] params, double[] expected) {
+        this.from = from;
+        this.to = to;
+        this.params = params;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {1, 2, new double[]{2.2, 1.3}, new double[]{4.4, 2.6}},
+                {3, 1, new double[]{3, 6}, new double[]{1, 2}},
+                {2, 3, new double[]{2, 3}, new double[]{3, 4.5}},
+        });
+    }
+
     @Test
-    public void testRescaleIngredients_toDouble() throws Exception {
-        List<Ingredient> ingredients = getIngredientList(2.2, 1.3);
+    public void testRescaleIngredients() throws Exception {
+        List<Ingredient> ingredients = getIngredientList(params);
 
         RecipeRescaleEngine engine = new RecipeRescaleEngine();
 
-        List<Ingredient> rescaledIngredients = engine.rescaleIngredientsToNumberOfHelpings(ingredients, 1, 2);
+        List<Ingredient> rescaledIngredients = engine.rescaleIngredientsToNumberOfHelpings(ingredients, from, to);
 
-        assertIngredientQuantity(rescaledIngredients, 4.4, 2.6);
+        assertIngredientQuantity(rescaledIngredients, expected);
     }
 
     private void assertIngredientQuantity(List<Ingredient> rescaledIngredients, double... expected) {
@@ -44,7 +70,7 @@ public class RecipeRescaleEngineTests {
         }
         return ret;
     }
-
+/*
     @Test
     public void testRescaleIngredients_toThirds() throws Exception {
         List<Ingredient> ingredients = getIngredientList(3, 6);
@@ -55,5 +81,5 @@ public class RecipeRescaleEngineTests {
 
         assertIngredientQuantity(rescaledIngredients, 1, 2);
     }
-
+*/
 }
