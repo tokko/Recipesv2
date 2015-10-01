74156e39a7d2eef99e9366f231b6a77a4daae276
tokko <andreg@kth.se>
2015-10-01 22:13:58 +0200

    rename method type

--- backend/src/main/java/com/tokko/recipesv2/backend/endpoints/RecipesApi.java
+++ backend/src/main/java/com/tokko/recipesv2/backend/endpoints/RecipesApi.java
@@ -164,7 +164,7 @@ public class RecipesApi {
             name = "generateShoppingList",
             path = "shoppingListrwggwa",
             httpMethod = ApiMethod.HttpMethod.GET)
-    public ShoppingList gemerateShoppingList(@Named("date") Long date, User user) {
+    public ShoppingList generateShoppingList(@Named("date") Long date, User user) {
         return shoppingListManager.generateShoppingList(date, user.getEmail());
     }
 
