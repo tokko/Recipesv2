b81a756792ae5c21503fe199c2467fcb82f5425f
tokko <andreg@kth.se>
2015-10-10 13:23:42 +0200

    restructurgin

--- backend/src/test/java/unit/engines/QuantityCalculatorEngineTests.java
+++ backend/src/test/java/unit/engines/QuantityCalculatorEngineTests.java
@@ -78,7 +78,8 @@ public class QuantityCalculatorEngineTests {
         assertEquals(1, q.getQuantity(), 0.02); //0.02 is acceptable error margin
         assertEquals(Unit.TEASPOON, q.getUnit());
     }
-     @Test
+
+    @Test
     public void testUpQuantity_MlIsMoreThan1Tablespoon_IsTablespoon() throws Exception{
         Quantity q1 = new Quantity(Unit.ML, 14.7868);
         Quantity q = engine.upQuantity(q1);
